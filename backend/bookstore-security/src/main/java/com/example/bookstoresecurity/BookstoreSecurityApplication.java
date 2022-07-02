e4a0a9ceeef6ab4482f12783fa23b530db34caa6
Widliwoo <shubinds@rambler.ru>
2022-07-02 23:49:29 +0300

    Добавил модуль с безопасностью

---
+++ backend/bookstore-security/src/main/java/com/example/bookstoresecurity/BookstoreSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstoresecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreSecurityApplication.class, args);
+    }
+
+}
