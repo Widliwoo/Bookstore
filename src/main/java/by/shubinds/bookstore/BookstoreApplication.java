079b574d7138d24576ff53ab987857ba75f7ae49
Widliwoo <shubinds@rambler.ru>
2022-07-15 15:44:25 +0300

    Импорт заготовки для проекта

---
+++ src/main/java/by/shubinds/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package by.shubinds.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
